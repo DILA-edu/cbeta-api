b641bf9b2d678078ca6f8cb78270993f4dab8fd4
Ray Chou <zhoubx@gmail.com>
2021-03-04 10:59:23 +0800

    fix bug

--- lib/tasks/quarterly/config.rb
+++ lib/tasks/quarterly/config.rb
@@ -1,5 +1,5 @@
 module Config
-  def get_config(env)
+  def get_config(env=nil)
     r = { v: 15 }
     r[:q1] = '2020Q4' # 製作 change log 時比對 q1, q2
     r[:q2] = '2021Q1'
@@ -12,12 +12,14 @@ module Config
     when 'production'
       r[:git]           = '/home/ray/git-repos'
       r[:old]           = "/var/www/cbdata#{r[:v]-1}/shared"
+      r[:old_data]      = File.join(r[:old],  'data')
       r[:root]          = "/var/www/cbdata#{r[:v]}/shared"
       r[:change_log]    = '/home/ray/cbeta-change-log'
       r[:ebook_convert] = '/usr/bin/ebook-convert'
     when 'development'
       r[:git]           = '/Users/ray/git-repos'
       r[:old]           = "/Users/ray/git-repos/cbdata12"
+      r[:old_data]      = File.join(r[:old],  'data')
       r[:root]          = "/Users/ray/git-repos/cbdata13"
       r[:change_log]    = '/Users/ray/Documents/Projects/CBETA/ChangeLog'
       r[:ebook_convert] = '/Applications/calibre.app/Contents/MacOS/ebook-convert'
@@ -27,7 +29,6 @@ module Config
     end
 
     r[:data]     = File.join(r[:root], 'data')
-    r[:old_data] = File.join(r[:old],  'data')
     r[:public]   = File.join(r[:root], 'public')
     r[:juanline] = File.join(r[:data], 'juan-line')
     r[:figures]  = Rails.configuration.x.figures
