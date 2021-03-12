ba3b256d0b1a4cadfe67c0a21f25baeeedaf198e
Ray Chou <zhoubx@gmail.com>
2021-03-12 10:30:50 +0800

    link_files: 取消 secrets.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 # set :log_level, :debug
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/secrets.yml', 'config/database.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('data', 'public/download', 'public/help')
