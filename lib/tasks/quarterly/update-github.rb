9c2b259fb49c0a15cb6f26a2997ef2f332f09847
Ray Chou <zhoubx@gmail.com>
2021-07-06 15:09:34 +0800

    git clone repo if not exist

--- lib/tasks/quarterly/update-github.rb
+++ lib/tasks/quarterly/update-github.rb
@@ -1,11 +1,29 @@
+REPOS = {
+  'CBR2X-figures' => 'git@github.com:cbeta-git/CBR2X-figures.git',
+  'ebook-covers' => 'git@github.com:cbeta-org/ebook-covers.git',
+  'gaiji-CB' => 'git@github.com:cbeta-org/gaiji-CB.git',
+  'rj-gif' => 'git@github.com:cbeta-org/rj-gif.git',
+  'sd-gif' => 'git@github.com:cbeta-org/sd-gif.git'
+}
+
 def pull(repo, tag=nil)
   folder = File.join($base, repo)
   puts '--'
-  puts "git pull #{folder}"
-  Dir.chdir(folder) do
-    system('git pull')
-    unless tag.nil?
-      system("git checkout tags/#{tag}")
+  if Dir.exist?(folder)
+    puts "git pull #{folder}"
+    Dir.chdir(folder) do
+      system('git pull')
+      unless tag.nil?
+        system("git checkout tags/#{tag}")
+      end
+    end
+  else
+    puts "git clone #{folder}"
+    Dir.chdir($base) do
+      system("git clone #{REPOS[repo]}")
+      unless tag.nil?
+        system("git checkout tags/#{tag}")
+      end
     end
   end
 end
@@ -24,7 +42,7 @@ if ENV['RAILS_ENV'] == 'cn'
 else
   pull('cbeta-xml-p5a')
   pull('gaiji-CB') # https://github.com/cbeta-org/gaiji-CB
-  pull('ebook-covers') # https://github.com/cbeta-org/ebook-covers
+  pull('ebook-covers') # 
   pull('sd-gif') # https://github.com/cbeta-org/sd-gif
   pull('rj-gif') # https://github.com/cbeta-org/rj-gif
 end
