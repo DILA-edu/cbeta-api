ffd00023474c7b861b5fc9b566900f1102c0c278
Ray Chou <zhoubx@gmail.com>
2021-11-03 15:15:37 +0800

    GB000 不應有 掃描圖檔 連結

--- lib/tasks/x2h_for_ui.rb
+++ lib/tasks/x2h_for_ui.rb
@@ -1013,7 +1013,7 @@ class P5aToHTMLForUI
       end
     when 'GA', 'GB'
       v = @vol[2..-1]
-      if @lb.end_with? 'a01'
+      if v != '000' and @lb.end_with?('a01')
         n = @lb[0, 4]
         r = %(<a class="facsimile" data-ref="#{e['ed']}v#{v}p#{n}"></a>)
       end
