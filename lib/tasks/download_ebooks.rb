e8ea504bf09c301ef67b82e266e0cbbfd1954f8d
Ray Chou <zhoubx@gmail.com>
2023-12-07 12:40:30 +0800

    cbeta 製作的 epub zip 解開不含目錄名

--- lib/tasks/download_ebooks.rb
+++ lib/tasks/download_ebooks.rb
@@ -7,8 +7,8 @@ class DownloadEbooks
 
   def run(type=nil)
     @errors = ""
-    dest_folder = Rails.root.join('data', 'download')
-    Dir.chdir(dest_folder) do
+    @dest_folder = Rails.root.join('data', 'download')
+    Dir.chdir(@dest_folder) do
       case type
       when 'epub', 'mobi'
         download_one_zip(type)
@@ -30,18 +30,37 @@ class DownloadEbooks
     i = 1
     loop do
       puts "#{i} ----------"
-      puts(cmd)
-      break if system(cmd)
+      break if exec(cmd)
     end
   end
 
   def download_one_zip(type)
-    download("#{BASE}/#{type}/cbeta_#{type}_#{@q}.zip")
-    File.rename("cbeta_#{type}_#{@q}.zip", "cbeta-#{type}-#{@q}.zip")
+    dest = "cbeta-#{type}-#{@q}.zip"
+
+    go = true
+    if File.exist?(dest)
+      puts "#{dest} 已存在，是否仍要重新下載？"
+      print 'Continue? 按 Enter 繼續，按 s 跳過 '
+      c = STDIN.getch.chomp
+      puts
+      go = false if c == 's'
+    end
+
+    if go
+      download("#{BASE}/#{type}/cbeta_#{type}_#{@q}.zip") 
+      File.rename("cbeta_#{type}_#{@q}.zip", dest)
+    end
+    
     fn = "cbeta-#{type}-#{@q}.zip"
-    if system("unzip #{fn}")
-      system "rm -rf #{type}"
-      system "mv cbeta_#{type}_#{@q} #{type}"
+    if exec("unzip #{fn} -d tmp")
+      exec("rm -rf #{type}")
+      d = "tmp/cbeta_#{type}_#{@q}"
+      if Dir.exist?(d)
+        exec("mv #{d} #{type}")
+        exec("rm -rf tmp")
+      else
+        exec("mv tmp #{type}")
+      end
       true
     else
       @errors << "解壓縮失敗: #{fn}\n"
@@ -53,14 +72,19 @@ class DownloadEbooks
     (1..3).each do |i|
       download("#{BASE}/pdf_a4/cbeta_pdf_#{i}_#{@q}.zip")
       File.rename("cbeta_pdf_#{i}_#{@q}.zip", "cbeta-pdf-#{@q}-#{i}.zip")
-      system "unzip cbeta-pdf-#{@q}-#{i}.zip"
+      exec("unzip cbeta-pdf-#{@q}-#{i}.zip")
     end
 
-    system "rm -rf pdf"
-    system "mv cbeta_pdf_1_#{@q} pdf"
-    system "mv cbeta_pdf_2_#{@q}/* pdf"
-    system "mv cbeta_pdf_3_#{@q}/* pdf"
-    system "rm -r cbeta_pdf_2_#{@q}"
-    system "rm -r cbeta_pdf_3_#{@q}"
+    exec("rm -rf pdf")
+    exec("mv cbeta_pdf_1_#{@q} pdf")
+    exec("mv cbeta_pdf_2_#{@q}/* pdf")
+    exec("mv cbeta_pdf_3_#{@q}/* pdf")
+    exec("rm -r cbeta_pdf_2_#{@q}")
+    exec("rm -r cbeta_pdf_3_#{@q}")
+  end
+
+  def exec(cmd)
+    puts cmd
+    system(cmd)
   end
 end
