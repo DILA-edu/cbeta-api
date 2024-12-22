211b2d7f4da4feeaed81f83a38a995d263a57579
Ray Chou <zhoubx@gmail.com>
2024-12-22 16:36:12 +0800

    fix bug

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -438,9 +438,11 @@ class SearchController < ApplicationController
       log_info "results size: #{r[:results].size}"
       # 回傳 行首資訊
       r[:results].each do |juan|
-        juan[:kwics][:results].each do |kwic|
-          file_basename = CBETA.get_xml_file_from_vol_and_work(kwic['vol'], juan[:work])
-          kwic[:linehead] = CBETA.get_linehead(file_basename, kwic['lb'])
+        if juan.key?(:kwics)
+          juan[:kwics][:results].each do |kwic|
+            file_basename = CBETA.get_xml_file_from_vol_and_work(kwic['vol'], juan[:work])
+            kwic[:linehead] = CBETA.get_linehead(file_basename, kwic['lb'])
+          end
         end
       end
     else
