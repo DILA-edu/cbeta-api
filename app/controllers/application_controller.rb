823a6ae9e584a48d96a98ff69c47033336291201
Ray Chou <zhoubx@gmail.com>
2026-01-01 10:53:15 +0800

    log request fullpath

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -234,7 +234,7 @@ class ApplicationController < ActionController::Base
   def log_action_start
     @start_time = Time.now
     user_agent = UserAgent.parse(request.user_agent)
-    logger.info "Request #{request.path} from #{request.remote_ip} at #{@start_time}, referer: #{request.referer}, origin: #{request.origin}, user_agent: #{user_agent.platform}/#{user_agent.browser}/#{user_agent.version}"
+    logger.info "Request #{request.fullpath} from #{request.remote_ip} at #{@start_time}, referer: #{request.referer}, origin: #{request.origin}, user_agent: #{user_agent.platform}/#{user_agent.browser}/#{user_agent.version}"
     logger.debug "start #{controller_name}##{action_name} #{@start_time}"
     logger.debug params.inspect
   end
